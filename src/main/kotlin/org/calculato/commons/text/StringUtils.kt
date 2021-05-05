7bb0ed32473a5720a0ee6cd34b7a8670ff17f523
Alan Viana <alanviana.nascimento@gmail.com>
2021-05-05 10:40:38 -0300

    chore: add text functions

--- src/main/kotlin/org/calculato/commons/text/StringUtils.kt
+++ src/main/kotlin/org/calculato/commons/text/StringUtils.kt
@@ -24,3 +24,13 @@ fun String.plural(duration: Duration) = this + if (duration.hoursPart != 1.toLon
  * Typesafe creation of an empty string
  */
 fun emptyString() = ""
+
+/**
+ * Normalizes [this] removing the _ character
+ */
+fun <E : Enum<E>> E.normalizeText(): String = this.name.replace("_", " ").toLowerCase()
+
+/**
+ * Concatenates a zeros sequence to [this] at the beginning to the specified [Int]
+ */
+fun Int.format(leftZeros: Int) = toString().padStart(leftZeros, '0')
